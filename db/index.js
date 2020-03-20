63b1f3aa707c4411fd6f0e350084a3c71cbd587a
Ryan Hall <ryanhall999@gmail.com>
2020-03-20 19:32:07 -0400

    1st ryan

--- db/index.js
+++ db/index.js
@@ -1,20 +1,42 @@
-const client = require('./client');
+const client = require("./client");
 
-const { authenticate, compare, findUserFromToken, hash } = require('./auth');
+const { authenticate, compare, findUserFromToken, hash } = require("./auth");
 
-const models = { products, users, orders, lineItems } = require('./models');
+const models = ({ products, users, orders, lineItems } = require("./models"));
+
+const faker = require("faker");
 
 const {
-  getCart,
-  getOrders,
-  addToCart,
-  removeFromCart,
-  createOrder,
-  getLineItems
-} = require('./userMethods');
+	getCart,
+	getOrders,
+	addToCart,
+	removeFromCart,
+	createOrder,
+	getLineItems
+} = require("./userMethods");
 
-const sync = async()=> {
-  const SQL = `
+const getProducts = amount => {
+	let products = [];
+	for (let i = 0; i < amount; i++) {
+		let prodName = faker.commerce.productName();
+		let price = faker.commerce.price(0.99, 20.0, 2);
+		let text = faker.lorem.sentence(5);
+		let rating = faker.random.number({ min: 55, max: 100 });
+		let img = faker.image.imageUrl(50, 50, "animals", true);
+		let newProd = {
+			name: prodName,
+			price: price,
+			description: text,
+			rating: rating,
+			image: img
+		};
+		products.push(newProd);
+	}
+	return products;
+};
+
+const sync = async () => {
+	const SQL = `
     CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
     DROP TABLE IF EXISTS "lineItems";
     DROP TABLE IF EXISTS orders;
@@ -31,15 +53,20 @@ const sync = async()=> {
     CREATE TABLE products(
       id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
       name VARCHAR(100) NOT NULL UNIQUE,
-      price DECIMAL NOT NULL,
+			price DECIMAL NOT NULL,
+			description VARCHAR(255),
+			rating INT,
+			image VARCHAR(255),
       CHECK (char_length(name) > 0)
-    );
+		);
+		
     CREATE TABLE orders(
       id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
       "userId" UUID REFERENCES users(id) NOT NULL,
       status VARCHAR(10) DEFAULT 'CART',
       "createdAt" TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-    );
+		);
+		
     CREATE TABLE "lineItems"(
       id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
       "orderId" UUID REFERENCES orders(id) NOT NULL,
@@ -47,79 +74,67 @@ const sync = async()=> {
       quantity INTEGER DEFAULT 1
     );
   `;
-  await client.query(SQL);
+	await client.query(SQL);
 
-  const _users = {
-    lucy: {
-      username: 'lucy',
-      password: 'LUCY',
-      role: 'ADMIN'
-    },
-    moe: {
-      username: 'moe',
-      password: 'MOE',
-      role: null
-    },
-    curly: {
-      username: 'larry',
-      password: 'LARRY',
-      role: null
-    },
-  };
+	const _users = {
+		lucy: {
+			username: "lucy",
+			password: "LUCY",
+			role: "ADMIN"
+		},
+		moe: {
+			username: "moe",
+			password: "MOE",
+			role: null
+		},
+		curly: {
+			username: "larry",
+			password: "LARRY",
+			role: null
+		}
+	};
 
-  const _products = {
-    foo: {
-      name: 'foo',
-      price: 2
-    },
-    bar: {
-      name: 'bar',
-      price: 2
-    },
-    bazz: {
-      name: 'bazz',
-      price: 2.50
-    },
-    quq: {
-      name: 'quq',
-      price: 11.99 
-    }
-  };
-  const [lucy, moe] = await Promise.all(Object.values(_users).map( user => users.create(user)));
-  const [foo, bar, bazz] = await Promise.all(Object.values(_products).map( product => products.create(product)));
+	const _products = getProducts(25);
 
-  const _orders = {
-    moe: {
-      userId: moe.id
-    },
-    lucy: {
-      userId: lucy.id
-    }
-  };
+	const [lucy, moe] = await Promise.all(
+		Object.values(_users).map(user => users.create(user))
+	);
+	const [foo, bar, bazz] = await Promise.all(
+		Object.values(_products).map(product => products.create(product))
+	);
 
-  const userMap = (await users.read()).reduce((acc, user)=> {
-    acc[user.username] = user;
-    return acc;
-  }, {});
-  const productMap = (await products.read()).reduce((acc, product)=> {
-    acc[product.name] = product;
-    return acc;
-  }, {});
-  return {
-    users: userMap,
-    products: productMap
-  };
+	const _orders = {
+		moe: {
+			userId: moe.id
+		},
+		lucy: {
+			userId: lucy.id
+		}
+	};
+
+	const userMap = (await users.read()).reduce((acc, user) => {
+		acc[user.username] = user;
+		return acc;
+	}, {});
+	const productMap = (await products.read()).reduce((acc, product) => {
+		acc[product.name] = product;
+		return acc;
+	}, {});
+	return {
+		users: userMap,
+		products: productMap
+	};
 };
 
 module.exports = {
-  sync,
-  models,
-  authenticate,
-  findUserFromToken,
-  getCart,
-  getOrders,
-  addToCart,
-  removeFromCart,
-  createOrder,
-  getLineItems
+	sync,
+	models,
+	authenticate,
+	findUserFromToken,
+	getCart,
+	getOrders,
+	addToCart,
+	removeFromCart,
+	createOrder,
+	getLineItems
 };
--- src/Products.js
+++ src/Products.js
@@ -1,30 +1,31 @@
-import React from 'react';
+import React from "react";
 
-const Products = ({ products, addToCart })=> {
-  return (
-    <div>
-      <h2>Products</h2>
-      <ul>
-        {
-          products.map( product => {
-            return (
-              <li key={ product.id }>
-                <span>
-                { product.name }
-                </span>
-                <span>
-                ${
-                  Number(product.price).toFixed(2)
-                }
-                </span>
-                <button onClick={ ()=> addToCart(product.id)}>Add to Cart</button>
-              </li>
-            );
-          })
-        }
-      </ul>
-    </div>
-  );
+const Products = ({ products, addToCart }) => {
+	return (
+		<div>
+			<h2>Products</h2>
+			<ul>
+				{products.map(product => {
+					return (
+						<li key={product.id}>
+							<div className="productBox">
+								<div className="productImg">
+									<img src={product.image}></img>
+								</div>
+								<div className="productText">
+									<span>{product.name}</span>
+									<span>Description: {product.description}</span>
+									<span>User Rating: {product.rating}</span>
+									<span>${Number(product.price).toFixed(2)}</span>
+								</div>
+							</div>
+							<button onClick={() => addToCart(product.id)}>Add to Cart</button>
+						</li>
+					);
+				})}
+			</ul>
+		</div>
+	);
 };
 
 export default Products;
