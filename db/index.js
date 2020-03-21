cd33ac07ed8e88af8e7d0475fc8cc40dcc696cc5
Chelsea Kramer <chelsea.kramer99@gmail.com>
2020-03-20 21:19:53 -0400

    still working on promos

--- db/index.js
+++ db/index.js
@@ -54,7 +54,7 @@ const sync = async () => {
 
     CREATE TABLE products(
       id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
-      name VARCHAR(100) NOT NULL UNIQUE,
+      name VARCHAR(100) NOT NULL,
 			price DECIMAL NOT NULL,
 			description VARCHAR(255),
 			rating INT,
