b2b4c3b19605720e17a1247bfcd6536278f2a813
Chelsea Kramer <chelsea.kramer99@gmail.com>
2020-03-23 16:48:48 -0400

    subtotal broken

--- db/index.js
+++ db/index.js
@@ -7,7 +7,6 @@ const models = ({ products, users, orders, lineItems } = require("./models"));
 const faker = require("faker");
 
 const {
-<<<<<<< HEAD
   getCart,
   getOrders,
   addToCart,
@@ -17,39 +16,30 @@ const {
   getLineItems,
   applyPromo,
   getAllPromos
-=======
-	getCart,
-	getOrders,
-	addToCart,
-	getPromo,
-	removeFromCart,
-	createOrder,
-	getLineItems
->>>>>>> master
 } = require("./userMethods");
 
 const getProducts = amount => {
-	let products = [];
-	for (let i = 0; i < amount; i++) {
-		let prodName = faker.commerce.productName();
-		let price = faker.commerce.price(0.99, 20.0, 2);
-		let text = faker.lorem.sentence(5);
-		let rating = faker.random.number({ min: 55, max: 100 });
-		let img = faker.image.imageUrl(50, 50, "animals", true);
-		let newProd = {
-			name: prodName,
-			price: price,
-			description: text,
-			rating: rating,
-			image: img
-		};
-		products.push(newProd);
-	}
-	return products;
+  let products = [];
+  for (let i = 0; i < amount; i++) {
+    let prodName = faker.commerce.productName();
+    let price = faker.commerce.price(0.99, 20.0, 2);
+    let text = faker.lorem.sentence(5);
+    let rating = faker.random.number({ min: 55, max: 100 });
+    let img = faker.image.imageUrl(50, 50, "animals", true);
+    let newProd = {
+      name: prodName,
+      price: price,
+      description: text,
+      rating: rating,
+      image: img
+    };
+    products.push(newProd);
+  }
+  return products;
 };
 
 const sync = async () => {
-	const SQL = `
+  const SQL = `
     CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
     DROP TABLE IF EXISTS addresses;
     DROP TABLE IF EXISTS "lineItems";
@@ -114,73 +104,72 @@ const sync = async () => {
     INSERT INTO promos (code, description, multiplier) VALUES ('UNF40', 'take 40% off any purchase', '0.6');
   `;
 
-	await client.query(SQL);
+  await client.query(SQL);
 
-	const _users = {
-		lucy: {
-			username: "lucy",
-			password: "LUCY",
-			role: "ADMIN"
-		},
-		moe: {
-			username: "moe",
-			password: "MOE",
-			role: null
-		},
-		curly: {
-			username: "larry",
-			password: "LARRY",
-			role: null
-		}
-	};
+  const _users = {
+    lucy: {
+      username: "lucy",
+      password: "LUCY",
+      role: "ADMIN"
+    },
+    moe: {
+      username: "moe",
+      password: "MOE",
+      role: null
+    },
+    curly: {
+      username: "larry",
+      password: "LARRY",
+      role: null
+    }
+  };
 
-	const _products = getProducts(25);
+  const _products = getProducts(25);
 
-	const [lucy, moe] = await Promise.all(
-		Object.values(_users).map(user => users.create(user))
-	);
-	const [foo, bar, bazz] = await Promise.all(
-		Object.values(_products).map(product => products.create(product))
-	);
+  const [lucy, moe] = await Promise.all(
+    Object.values(_users).map(user => users.create(user))
+  );
+  const [foo, bar, bazz] = await Promise.all(
+    Object.values(_products).map(product => products.create(product))
+  );
 
-	const _orders = {
-		moe: {
-			userId: moe.id
-		},
-		lucy: {
-			userId: lucy.id
-		}
-	};
+  const _orders = {
+    moe: {
+      userId: moe.id
+    },
+    lucy: {
+      userId: lucy.id
+    }
+  };
 
-	const userMap = (await users.read()).reduce((acc, user) => {
-		acc[user.username] = user;
-		return acc;
-	}, {});
-	const productMap = (await products.read()).reduce((acc, product) => {
-		acc[product.name] = product;
-		return acc;
-	}, {});
-	return {
-		users: userMap,
-		products: productMap
-	};
+  const userMap = (await users.read()).reduce((acc, user) => {
+    acc[user.username] = user;
+    return acc;
+  }, {});
+  const productMap = (await products.read()).reduce((acc, product) => {
+    acc[product.name] = product;
+    return acc;
+  }, {});
+  return {
+    users: userMap,
+    products: productMap
+  };
 };
 
 const addAddress = async (address, user) => {
-	const SQL =
-		"INSERT INTO addresses(CustomerId, address, city, state, zip) values($1, $2, $3, $4, $5) returning *";
-	return (
-		await client.query(SQL, [
-			user.id,
-			address.address,
-			address.city,
-			address.state,
-			address.zip
-		])
-	).rows[0];
+  const SQL =
+    "INSERT INTO addresses(CustomerId, address, city, state, zip) values($1, $2, $3, $4, $5) returning *";
+  return (
+    await client.query(SQL, [
+      user.id,
+      address.address,
+      address.city,
+      address.state,
+      address.zip
+    ])
+  ).rows[0];
 };
 module.exports = {
-<<<<<<< HEAD
   sync,
   models,
   authenticate,
@@ -194,18 +183,4 @@ module.exports = {
   getLineItems,
   applyPromo,
   getAllPromos
-=======
-	sync,
-	models,
-	authenticate,
-	findUserFromToken,
-	getCart,
-	getOrders,
-	getPromo,
-	addToCart,
-	removeFromCart,
-	createOrder,
-	getLineItems,
-	addAddress
->>>>>>> master
 };
